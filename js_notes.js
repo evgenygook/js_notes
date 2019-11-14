b6ed13aed45bd2b948c21f3b0b37a25be2645d94
Evgeniy Kirillov <kirillov@iwayex.com>
2019-11-14 18:39:13 +0700

    добавил генератор слов

--- js_notes.js
+++ js_notes.js
@@ -10,3 +10,19 @@ var curr_year = d.getFullYear();
   tdy = curr_year + "-" + curr_month + "-" + curr_date
   return tdy;
 }
+
+//генератор слов
+
+function getWord (id) {
+ var words = [
+  'rock', 'paper', 'scissors', 'megaerror', 'escapology',
+  'brightwork', 'verkrampte', 'protectrix', 'nudibranch', 'grandchild',
+  'newfangled', 'flugelhorn', 'mythologer', 'pluperfect', 'jellygraph',
+  'quickthorn', 'rottweiler', 'technician', 'cowpuncher', 'middlebrow',
+  'jackhammer', 'triphthong', 'wunderkind', 'dazzlement', 'jabberwock',
+  'witchcraft', 'pawnbroker', 'thumbprint', 'motorcycle', 'cryptogram'
+ ];
+
+let result = words[Math.floor(Math.random()*words.length)];
+   return result;
+}
