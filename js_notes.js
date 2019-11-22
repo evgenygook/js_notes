8cb38920634b84a48fb92f315fed4a364c4c1b9b
Evgeniy Kirillov <kirillov@iwayex.com>
2019-11-22 13:35:13 +0700

    Изменил генератор uid

--- js_notes.js
+++ js_notes.js
@@ -6,21 +6,24 @@ var d = new Date();
 var curr_date = d.getDate();
 var curr_month = d.getMonth() + 1;
 var curr_year = d.getFullYear();
-  
+
   tdy = curr_year + "-" + curr_month + "-" + curr_date
   return tdy;
 }
 
+//
+
+
 //генератор слов
 
-function getWord (id) {
- var words = [
-  'rock', 'paper', 'scissors', 'megaerror', 'escapology',
-  'brightwork', 'verkrampte', 'protectrix', 'nudibranch', 'grandchild',
-  'newfangled', 'flugelhorn', 'mythologer', 'pluperfect', 'jellygraph',
-  'quickthorn', 'rottweiler', 'technician', 'cowpuncher', 'middlebrow',
-  'jackhammer', 'triphthong', 'wunderkind', 'dazzlement', 'jabberwock',
-  'witchcraft', 'pawnbroker', 'thumbprint', 'motorcycle', 'cryptogram'
+function getWord () {
+ let words = [
+  'go', 'fokk', 'yourself', 'normies', 'bitards',
+  'kenny', '2ch', 'loli', 'anime', 'bokunopiko',
+  'iway', 'love', 'like', 'hate', 'people',
+  'pornhub', 'twitter', 'rofl', 'sorry', 'dont',
+  'speak', 'human', 'life', 'is', 'bautiful',
+  'end', 'dead', 'poop', 'restapi', 'shit'
  ];
 
 let result = words[Math.floor(Math.random()*words.length)];
